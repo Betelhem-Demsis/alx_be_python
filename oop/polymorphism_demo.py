326bf035e26fe894cf783d222bd3c853ba049b80
betelhem demsis <bettydemsis@gmail.com>
2024-07-18 14:38:14 +0300

    polymorphism

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -4,16 +4,17 @@ import math
 class Shape:
     def area(self):
        raise NotImplementedError("derived classes need to override this method")
+    
+class Circle(Shape):
+    def __init__(self,radius):
+        super().__init__()
+        self.radius=radius
+    def area(self):
+        return  math.pi * (self.radius ** 2)
 class Rectangle(Shape):
     def __init__(self,length,width):
         super().__init__()
         self.length=length
         self.width=width
     def area(self):
-        return f"The area of the Rectangle is: {self.length*self.width}"
-class Circle(Shape):
-    def __init__(self,radius):
-        super().__init__()
-        self.radius=radius
-    def area(self):
-        return f"The area of the Circle is: { math.pi * (self.radius**2)}"
+        return self.length*self.width
